ce66666f78fe65e0f4a58e1ab4e8d535197f49a7
Feporras <67969032+Feporras@users.noreply.github.com>
2020-07-23 11:35:14 -0500

    Add files via upload

---
+++ thursday july 23 2020.playground/Contents.swift
@@ -0,0 +1,9 @@
+struct carEngines {
+    var engineOne = "A 4 cylinder engine contains 4 pistons"
+    var engineTwo = "A V6 works like a pair of three-cylinder engines joined together in a V-like shape. That's where the 'V' in V6 comes from. The 6 represents the number of cylinders. "
+    var engineThree = "V8 it has The V stands for the shape of the engine, and the 8 stands for the number of cylinders in the engine. So, a V8 engine means a V-shaped engine with a total of 8 cylinders."
+    
+}
+let meaningsForCar = carEngines()
+
+print("There are a lot of different engine possibilities when buying a car , three possibilities are V4 , V6 and V8.\n In the first engine we have , \(meaningsForCar.engineOne).\n  The second engine is \(meaningsForCar.engineTwo).\n The third engine is \(meaningsForCar.engineThree).\n These are your engione options.")
